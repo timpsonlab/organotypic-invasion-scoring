6e3aa123cda380e2d64dc78c2c146edea6925729
Sean Warren <sean.warren09@imperial.ac.uk>
2016-01-18 23:20:39 +1100

    move product into folder for distribution

--- organotypic-invasion-scoring/Compile.m
+++ organotypic-invasion-scoring/Compile.m
@@ -3,7 +3,8 @@ function Compile
     % Get version
     [~,ver] = system('git describe','-echo');
     ver = ver(1:end-1);
-    
+    is_release = isempty(regexp(ver,'-\d-+[a-z0-9]+','ONCE'));
+
     % Build App
     try
     rmdir('build','s');
@@ -21,15 +22,26 @@ function Compile
         ext = '.app';
     end
    
-    new_file = ['Organotypic Invasion Scoring ' ver];
+    new_file = ['Organotypic Invasion Scoring ' ver ' ' computer('arch')];
     movefile(['build' filesep 'Organotypic_Invasion_Scoring' ext], ['build' filesep new_file ext]);
     
     if ismac
         mkdir(['build' filesep 'dist']);
         movefile(['build' filesep new_file ext], ['build' filesep 'dist' filesep new_file ext]);
         cmd = ['hdiutil create "./build/' new_file '.dmg" -srcfolder ./build/dist/ -volname "' new_file '" -ov'];
-        disp(cmd)
         system(cmd)
+        final_file = ['build/' new_file '.dmg'];
+    else
+        final_file = ['build' filesep new_file ext];
     end
-
     
+    if is_release
+        dir1 = 'release';
+    else 
+        dir1 = 'latest';
+    end
+    mkdir(['build' filesep dir1]);
+    mkdir(['build' filesep dir1 filesep ver]);
+    
+    movefile(final_file, ['build' filesep dir1 filesep ver]);
+end
