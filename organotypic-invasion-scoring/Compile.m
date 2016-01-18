f22966312be979183a460adbeb44cd2d88138a47
Sean Warren <sean.warren09@imperial.ac.uk>
2016-01-18 17:22:45 +1100

    Make disk image on Mac

--- organotypic-invasion-scoring/Compile.m
+++ organotypic-invasion-scoring/Compile.m
@@ -2,8 +2,14 @@ function Compile
     
     % Get version
     [~,ver] = system('git describe','-echo');
+    ver = ver(1:end-1);
     
     % Build App
+    try
+    rmdir('build','s');
+    catch
+    end
+
     mkdir('build');
     delete(['build' filesep '*']);
     mcc('-m','Interface.m', ...
@@ -15,5 +21,15 @@ function Compile
         ext = '.app';
     end
    
-    movefile(['build' filesep 'Organotypic_Invasion_Scoring' ext], ['build' filesep 'Organotypic Invasion Scoring ' ver ext]);
-        
+    new_file = ['Organotypic Invasion Scoring ' ver];
+    movefile(['build' filesep 'Organotypic_Invasion_Scoring' ext], ['build' filesep new_file ext]);
+    
+    if ismac
+        mkdir(['build' filesep 'dist']);
+        movefile(['build' filesep new_file ext], ['build' filesep 'dist' filesep new_file ext]);
+        cmd = ['hdiutil create "./build/' new_file '.dmg" -srcfolder ./build/dist/ -ov'];
+        disp(cmd)
+        system(cmd)
+    end
+
+    
